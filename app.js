835fec01807c7b80f074749e3e18f17d4431ebc1
Romiec13 <romiec13@gmail.com>
2022-03-18 21:18:26 -0500

    Started to add JS for results of game
    Need to go back and look at syntax of function getResults as nothing is popping up on the webpage.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
+const resultsDisplay = document.getElementById('game-results')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let results
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   userChoice = e.target.id
@@ -25,3 +26,30 @@ function generateComputerChoice() {
     }
     computerChoiceDisplay.innerHTML = computerChoice
 }
+
+function getResults () {
+  if(computerChoice === userChoice){
+   results ='IT IS A DRAW!'
+  }
+  if (computerChoice === 'rock' && userChoice === "paper"){
+    results = 'YOU WIN!'
+  }
+  if (computerChoice === 'rock' && userChoice === "scissors"){
+    results = 'YOU LOSE ☹'
+  }
+  if (computerChoice === 'paper' && userChoice === "scissors"){
+    results = 'YOU WIN!'
+  }
+  if (computerChoice === 'paper' && userChoice === "rock") {
+    results = 'YOU LOSE ☹'
+  }
+  if(computerChoice === 'scissors' && userChoice === "rock"){
+    results = 'YOU WIN!'
+  }
+  if(computerChoice === 'scissors' && userChoice === "paper"){
+    results = 'YOU LOSE ☹'
+  }
+  resultsDisplay.innerHTML = results
+}
+
+
